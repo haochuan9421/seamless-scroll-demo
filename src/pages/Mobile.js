979f29a7635603beacb68ce441959f7559ad888f
HaoChuan9421 <hc199421@gmail.com>
2019-03-23 14:15:33 +0800

    完成基本设计

---
+++ src/pages/Mobile.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import './Mobile.scss';
+
+class Mobile extends Component {
+  render() {
+    return <div className="mobile-page">Mobile</div>;
+  }
+}
+
+export default Mobile;
---
+++ src/pages/PC.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import './PC.scss';
+
+class PC extends Component {
+  render() {
+    return <div className='pc-page'>PC</div>;
+  }
+}
+
+export default PC;
