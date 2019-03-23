1d1b5d33891b3f3c52e6ca0c3baeb6d04dfbb937
HaoChuan9421 <hc199421@gmail.com>
2019-03-24 04:58:50 +0800

    Updates

---
+++ precache-manifest.d96c041df34bbdb77fb0e99d197bd546.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "7f3ea01d9ee01301f081",
+    "url": "/seamless-scroll-demo/static/js/runtime~main.0bd45d7f.js"
+  },
+  {
+    "revision": "259263197184862acf3b",
+    "url": "/seamless-scroll-demo/static/js/main.2393f8e7.chunk.js"
+  },
+  {
+    "revision": "77d2263fd55e82ef0227",
+    "url": "/seamless-scroll-demo/static/js/2.1bd46548.chunk.js"
+  },
+  {
+    "revision": "259263197184862acf3b",
+    "url": "/seamless-scroll-demo/static/css/main.3e450bd8.chunk.css"
+  },
+  {
+    "revision": "d561ca4232c719ff71e00e904dbbae1f",
+    "url": "/seamless-scroll-demo/index.html"
+  }
+];
